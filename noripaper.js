9f0f7fa0412ed4d0b3ebd12bae3166cdfe9c66db
hashrock <hashedrock@gmail.com>
2015-05-15 16:58:45 +0900

    Realtime sync

--- noripaper.js
+++ noripaper.js
@@ -1,8 +1,14 @@
 Documents = new Meteor.Collection("documents")
+Docs = new Meteor.Collection("docs")
+
+
+
+
 
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
+  Session.setDefault('editingDocId', "");
 
   Template.hello.helpers({
     counter: function () {
@@ -10,38 +16,74 @@ if (Meteor.isClient) {
     },
     documents: function(){
       return Documents.find();
+    },
+    editingDocId: function(){
+      return Session.get('editingDocId');
+    },
+    isEditing: function(){
+      return Session.get('editingDocId') !== "";
+      
+      //return Session.get('editingDocId') === this._id;
+    },
+    config: function(){
+      return function(cm){
+        cm.setOption("theme", "default")
+        cm.setOption("lineNumbers", true)
+        cm.setOption("lineWrapping", true)
+        cm.setOption("smartIndent", true)
+        cm.setOption("indentWithTabs", true)
+        cm.setOption("defaultTextHeight", 100);
+        cm.on("change", function(evt){
+          var id = Session.get('editingDocId');
+          Meteor.call("getDocumentText", id, function(err, snapshot){
+            Documents.update(
+              {_id: id},
+              {$set: {text: snapshot}}
+            )
+          });
+        });
+      }
     }
   });
   
-  Template.hello.config = function(){
-    return function(cm){
-      cm.setOption("theme", "default")
-      cm.setOption("lineNumbers", true)
-      cm.setOption("lineWrapping", true)
-      cm.setOption("smartIndent", true)
-      cm.setOption("indentWithTabs", true)
-      cm.setOption("defaultTextHeight", 100);
-    }
-  }
-
   Template.hello.events({
     'click #addNew': function () {
       Documents.insert({
-        text: "HEllo!"
-      })
-      // increment the counter when button is clicked
-      //Session.set('counter', Session.get('counter') + 1);
+        text: ""
+      });
     },
     'click .remove': function () {
       Documents.remove(this._id);
-      // increment the counter when button is clicked
-      //Session.set('counter', Session.get('counter') + 1);
+    },
+    'click .edit': function () {
+      Session.set('editingDocId', this._id);
+    },
+    'click .endedit': function (evt) {
+      var id = Session.get('editingDocId');
+      Meteor.call("getDocumentText", id, function(err, snapshot){
+        console.log(id);
+        console.log(snapshot);
+        Documents.update(
+          {_id: id},
+          {$set: {text: snapshot}}
+        )
+        Session.set('editingDocId', "");
+      });
     }
   });
 }
 
+
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.methods({
+      getDocumentText: function (docid) {
+        var getSnapshot = Meteor.wrapAsync(ShareJS.model.getSnapshot);
+        var doc = getSnapshot(docid);
+        return doc.snapshot;
+      }
   });
+  /*
+  Meteor.startup(function () {
+  });
+  */
 }
